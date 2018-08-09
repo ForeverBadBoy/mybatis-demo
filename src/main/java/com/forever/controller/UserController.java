0e59fdcd5f71e7a021183bd5c64b9c48c2365a45
ForeverBadBoy <1147275074@qq.com>
2018-08-10 00:28:22 +0800

    添加日志打印功能

--- src/main/java/com/forever/controller/UserController.java
+++ src/main/java/com/forever/controller/UserController.java
@@ -2,6 +2,7 @@ package com.forever.controller;
 
 import com.forever.model.User;
 import com.forever.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author LENOVO
  * @date 2018/08/09
  */
+@Slf4j
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
@@ -28,7 +30,7 @@ public class UserController {
     @ResponseBody
     @RequestMapping(value = "/all/{pageNum}/{pageSize}", produces = {"application/json;charset=UTF-8"})
     public Object findAllUser(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize) {
-
+        log.info("pageNum:{}, pageSize:{}", pageNum, pageSize);
         return userService.findAllUser(pageNum, pageSize);
     }
 }
